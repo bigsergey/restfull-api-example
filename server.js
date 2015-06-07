27b96d6f25aa1ec8b5694b8c7dc185ba5b09464e
sergey <bolszow@gmail.com>
2015-06-08 00:43:31 +0200

    update package.json, add mongoose to server

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+
+var routes = require('./app/routes/index');
+
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+
+mongoose.connect('mongodb://localhost/restapi', function(err) {
+    if (err) {
+        console.log('Database connection error', err);
+    } else {
+        console.log('Database connection successful');
+    }
+});
 
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;
-
-var routes = require('./app/routes/index');
 
 app.use('/api', routes);
 
