4bf4d60913aa63f8cd700e8559b8021e16fce5c7
sergey <bolszow@gmail.com>
2015-06-08 02:44:00 +0200

    simple login swagger documentation

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-
-var routes = require('./app/routes/index');
-
+var path = require('path');
+var http = require('http');
+var swagger = require('swagger-express');
+var api = require('./api');
 
 var app = express();
 var port = process.env.PORT || 8080;
 
+var mongoose = require('mongoose');
+var routes = require('./app/routes/index');
 
 mongoose.connect('mongodb://localhost/restapi', function(err) {
     if (err) {
@@ -19,13 +20,35 @@ mongoose.connect('mongodb://localhost/restapi', function(err) {
 });
 
 
+
+app.set('port', port);
+app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+app.use(swagger.init(app, {
+    apiVersion: '1.0',
+    swaggerVersion: '1.0',
+    basePath: 'http://localhost:8080',
+    swaggerURL: '/swagger',
+    swaggerJSON: '/api-docs.json',
+    swaggerUI: './public/swagger/',
+    apis: ['./api.js']
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.use('/api', routes);
 
-app.listen(port);
+app.post('/login', api.login);
 
-console.log('Server is listening on port ' + port + ' http://localhost:' + port);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+});
