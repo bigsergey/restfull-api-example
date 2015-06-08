b87663e2193357874efc8870fef7931b21957f63
sergey <bolszow@gmail.com>
2015-06-08 17:56:02 +0200

    saving new movie object to database with mongoose

--- handlers/movies.js
+++ handlers/movies.js
@@ -1,4 +1,7 @@
 'use strict';
+
+var Movie = require('./../models/movie');
+
 /**
  * Operations on /movies
  */
@@ -12,15 +15,18 @@ module.exports = {
         res.send(501);
     },
     /**
-     * 
+     *
      * parameters: body
      * produces: application/json
      */
-    post: function addMovie(req, res) {
-        res.send(501);
+    post: function addMovie(req, res, next) {
+        Movie.create(req.body, function (err, post) {
+            if (err) return next(err);
+            res.json(post);
+        });
     },
     /**
-     * 
+     *
      * parameters: body
      * produces: application/json
      */
--- models/movie.js
+++ models/movie.js
@@ -1,14 +1,17 @@
 'use strict';
 
-function movie(options) {
-    if (!options) {
-        options = {};
-    }
-    
-    this.id = options.id;
-    this.categories = options.categories;
-    this.title = options.title;
-    this.posterUrls = options.posterUrls;
-}
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-module.exports = movie;
+
+var MovieSchema = new Schema({
+	title: String,
+	posterUrls: [String],
+	categories: [Schema.Types.ObjectId]
+});
+
+MovieSchema.static('findByTitle', function (title, callback) {
+	return this.find({title: title}, callback);
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
